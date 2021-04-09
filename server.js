651dde3a5b44733acbde7ecc990c0848460fc341
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-04-09 17:18:39 +0100

    added the favicon stuff back

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
+const favicon = require('express-favicon');
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
@@ -29,10 +30,11 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
 app.use(session(sess));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on https//:locahost:${PORT}`));
+    app.listen(PORT, () => console.log(`Now listening on https://${PORT}`));
 });
