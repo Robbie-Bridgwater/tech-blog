881dafb8c95cad89b70e384ca5c1d41b99ff0861
Robbi <Robbie_Bridgwater@Hotmail.co.uk>
2021-04-09 17:14:44 +0100

    still removing bits of favicon to try and make it work

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-// const favicon = require('express-favicon');
 const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 const sequelize = require("./config/connection");
@@ -30,7 +29,6 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-// app.use(favicon(__dirname + '/public/assets/img/favicon.ico'));
 app.use(session(sess));
 
 app.use(routes);
